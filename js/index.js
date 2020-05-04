77bb10f82d4c08b30171e170db260e4b7a41ab42
Sunhwa Cheon <Sunhwa@CheonSuhwauiAir.JOOWIN>
2020-05-04 14:34:42 -0400

    0504

--- js/index.js
+++ js/index.js
@@ -1,39 +1,45 @@
-//EXERCISE 1: The Fortune Teller
-//Why pay a fortune teller when you can just program your fortune yourself? Store the following into variables: job title, geographic location, annual salary, company name. Output your fortune to the screen like so: "You will be a X in Y, making $N for Z."
-
-var JobTitle = 'professor';
-var Location = 'UK';
-var Salary = '53,000';
-var Company = 'Royal College of Art';
-
-console.log('You will be a ' + JobTitle + ' in ' + Location + ' and making $' + Salary + ' in ' + Company);
-
-//EXERCISE 2: The Age Calculator
-//Forgot how old someone is? Calculate it! Store the current year in a variable. Store their birth year in a variable. Calculate their age based on the stored values. Output them to the screen like so: "They are NN years old.", substituting the values.
-
-var CurrentYear = 2019 ;
-var BirthYear = 1992;
-var age= CurrentYear - BirthYear;
-console.log('He is ' + age +' years old.');
+const navButton = document.querySelector('.nav-button');
+const navOpen = document.querySelector('.nav-open');
+const close = document.querySelector('.close');
+const tl = new TimelineLite({paused: true, reversed :true});
 
 
-//EXERCISE 3: The Lifetime Supply Calculator
-//Ever wonder how much a "lifetime supply" of your favorite snack is? Wonder no more! Store your current age into a variable. Store a maximum age into a variable. Store an estimated amount per day (as a number). Calculate how many you would eat total for the rest of your life. Output the result to the screen like so: "You will need NN to last you until the ripe old age of X".
-var MyAge = 27;
-var MaxAge = 100;
-var PerDayUsage = 5;
-var NeededSupply = ((MaxAge-MyAge)*365)*PerDayUsage;
-console.log('You will need ' + NeededSupply + ' to last you until the ripe old age of ' + MaxAge);
+tl.to('.cover', 1,{
+    width: '60%', top: '0', objectPosition:'left', ease: Power2.easeOut
+})
+.to('nav', 1,{
+    height: '100%', ease: Power2.easeOut
+}, '-=0.5')
+.fromTo('.nav-open', 0.7, {
+    opacity:0, x: 50, ease: Power2.easeOut
+}, {
+    opacity:1, x: 0, onComplete : function () {
+        navOpen.style.pointerEvents = 'auto'
+    }
+    
+});
 
-//EXERCISE 4: The Geometrizer
-//Calculate properties of a circle, using the definitions here. Store a radius into a variable. Calculate the circumference based on the radius, and output "The circumference is NN". Calculate the area based on the radius, and output "The area is NN".
-var radius = 3;
-var circumference = Math.PI * 2*radius;
-var area = Math.PI * radius*radius;
+navButton.addEventListener('click', ()=>{
+    if(tl.isActive()){
+        e.preventDefault();
+        e.stopImmediatePropagation();
+        return false;
+    }
+    toggleTween(tl)
+});
 
-console.log("The circumference is " + circumference + "The area is " + area);
-//EXERCISE 5: The Temperature Converter
-//It's hot out! Let's make a converter based on the steps here. Store a celsius temperature into a variable. Convert it to fahrenheit and output "NN°C is NN°F".  Now store a fahrenheit temperature into a variable. Convert it to celsius and output "NN°F is NN°C."
-var fahrenheit=78;
-var temperature=(fahrenheit-32)*5/9;
-console.log(fahrenheit+'°F' + 'is' +temperature+'°C');
+
+
+close.addEventListener('click', ()=>{
+    if(tl.isActive()){
+        e.preventDefault();
+        e.stopImmediatePropagation();
+        return true;
+    }
+    toggleTween(tl)
+});
+
+function toggleTween(tween) {
+    tween.reversed() ? tween.play() : tween.reverse();
+}
+
---
+++ readme.md
@@ -0,0 +1,5 @@
+## User interface
+ - Main yellow : #e8c910
+ - Point blue : #104c63
+ - Dark grey : #474849
+ - Hover color : #
