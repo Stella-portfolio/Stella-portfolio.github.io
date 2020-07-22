4786d99d40837e99a968d9f34d04a94c63f9f782
Sunhwa Cheon <sunhwacheon@MacBook-Pro.local>
2020-07-22 13:44:51 -0400

    0722

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+const exphbs  = require('express-handlebars');
+
+
+
+// Set handlebar
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+// Set handlebar routes
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+//creat about page route
+app.get('/wetfood', function (req, res) {
+    res.render('wetfood', {title : 'Wet Food'});
+});
+
+app.get('/dryfood', function (req, res) {
+    res.render('dryfood', {title : 'Dry Food'});
+});
+
+app.get('/catnip', function (req, res) {
+    res.render('catnip', {title : 'catnip'});
+});
+
+// Set static folder
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.listen(PORT, ()=> console.log('server listening on port ' + PORT))
+
